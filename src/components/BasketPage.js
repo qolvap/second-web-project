9e3b89403ed88f30bcabed56e050775deda39aaf
Pavlo Grebennikov <pavlogrebennikov@MacBook-Air-Pavlo.local>
2023-06-20 23:13:43 +0200

    css changes in card section

--- src/components/BasketPage.js
+++ src/components/BasketPage.js
@@ -11,25 +11,29 @@ function BasketPage({ cartItems, setCartItems }) {
     <>
         <div className="categories--section">
           <div className="categories--head">
+          <Link to="/Categories" className="back--to--btn">
+                Back to shopping
+              </Link>
               <h1 className="categories--title">Your cart:</h1>
+                 
+            
+        
+      
+             
+          </div>
+        </div>
+      <div className="card--products">
                   {cartItems.map(item => (
                      <div key={item.id} className="cart-item">
                          <h2 className="shopping--card--name">{item.name}</h2>
                          <p className="shopping--card--describtion">{item.type}</p>
                          <p className="shopping--card--price">{item.price * item.quantity}€</p>
                          <p className="shopping--card--quanity">Quantity: {item.quantity}</p>
-                         <button className="shopping--card--remove" onClick={() => removeFromCart(item.id)}>Remove</button>
+                         <button className="shopping--card--remove" onClick={() => removeFromCart(item.id)}>X</button>
                      </div>
        
                   ))}
-            
-        
-      
-              <Link to="/Categories" className="back--to--btn">
-                Back to shopping
-              </Link>
-          </div>
-        </div>
+                  </div>
         
     </>
   );
